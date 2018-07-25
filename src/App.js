1bda47d6ac087eeeee6370cf6172c18b4c1971b3
Matija Galina <matija@krakensystems.co>
2018-07-25 21:31:13 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import MapContainer from './MapContainer';
 import Search from './Search';
 import Title from './Title';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
@@ -24,6 +25,9 @@ class App extends Component {
                 <MapContainer />
               </div>
             </div>
+            <div className='footer-container' >
+                <Footer />
+              </div>
           </div>
         )}
         />
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = ({}) => (
+  <div className='footer'>
+    <h3>
+      Test
+    </h3>
+  </div>
+);
+
+export default Footer;
