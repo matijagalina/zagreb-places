5b5312448cb18304a018158f1128c5de213b3b45
Matija Galina <matija@krakensystems.co>
2018-08-02 19:05:41 +0200

    add locations file, handle google map error

---
+++ src/Locations.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    name: 'Museum of Broken Relationships',
+    id: '4cab48e6f47ea14351508a21',
+    position: {
+      lat: 45.814921,
+      lng: 15.973434
+    }
+  },
+  {
+    name: 'Muzej Iluzija',
+    id: '4c8d04108018a1cd9d90f1d2',
+    position: {
+      lat: 45.812978,
+      lng: 15.966353
+    }
+  },
+  {
+    name: 'Tortureum- Museum of torture',
+    id: '558ae639498ea3f01851ff03',
+    position: {
+      lat: 45.814391,
+      lng: 15.975747
+    }
+  },
+  {
+    name: 'Park Maksimir',
+    id: '55a127d6498e2b2b8f75a31d',
+    position: {
+      lat: 45.819654,
+      lng: 16.015640
+    }
+  },
+  {
+    name: 'Botanical garden',
+    id: '583ca39da55db039f593aea1',
+    position: {
+      lat: 45.804944,
+      lng: 15.972202
+    }
+  }
+]
